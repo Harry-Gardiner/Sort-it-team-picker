290b358c1a9e8d71fbc76f45c904ab4f02dc08e7
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-26 17:26:27 +0000

    Delete component added to Player List

--- src/Components/PlayerList/PlayerList.js
+++ src/Components/PlayerList/PlayerList.js
@@ -1,11 +1,18 @@
-const PlayerList = ({ players }) => {
+const PlayerList = ({ players, handleClick }) => {
     return (
         <ul>
             { players.map((player, index) => {
                 return (
-                    <li key={index}>
-                        { player.name}
-                    </li>
+                    <>
+                        <li key={index}>
+                            {player.name}
+                        </li>
+                        <button
+                            onClick={() => handleClick(index)}
+                        >
+                            x
+                        </button>
+                    </>
                 )
             })}
         </ul>
