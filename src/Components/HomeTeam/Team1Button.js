29940aec1dbf0954d64c0c07a7a19fbdaecfeacc
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-29 19:02:11 +0000

    Home button component and wrappers created

---
+++ src/Components/HomeTeam/Team1Button.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import HomeTeamButton from './HomeTeamButton';
+import { toggleHomeTeam } from '../../data/actions'
+
+const mapStateToProps = ({ team1Home }) => {
+    return {
+        home: team1Home === true,
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        handleClick: () => { dispatch(toggleHomeTeam()) },
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeTeamButton);
