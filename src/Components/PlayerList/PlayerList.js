266c4a914cc938086b84abbd12c1fdebabeb8f79
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-29 12:12:32 +0000

    Lists updated to display player skill

--- src/Components/PlayerList/PlayerList.js
+++ src/Components/PlayerList/PlayerList.js
@@ -6,6 +6,7 @@ const PlayerList = ({ players, handleClick }) => {
                     <div key={index}>
                         <li>
                             {player.name}
+                            <span style={{ position: "absolute", right: "2rem" }}>skill: {player.skill}</span>
                         </li>
                         <button
                             onClick={() => handleClick(index)}
