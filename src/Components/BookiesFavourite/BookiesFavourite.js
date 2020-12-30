252a84d081e36066d464a8540058c211c4fbd595
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-30 17:41:45 +0000

    Added home advantage * to the score its applied too

--- src/Components/BookiesFavourite/BookiesFavourite.js
+++ src/Components/BookiesFavourite/BookiesFavourite.js
@@ -15,9 +15,9 @@ const BookiesFavourite = ({ team1Name, team2Name, teamsPicked, bookiesFavouriteI
                     {showBookiesFavourite === false ? null :
                         <div className="dropdown-content">
                             <p>{bookiesFavTeam === "team1" ? team1Name : team2Name} are tipped to win.</p>
-                            <p>{team1Name} skill total: {bookiesFavTeam === "team1" ? favTeamSkill : otherTeamSkill} </p>
-                            <p>{team2Name} skill total: {bookiesFavTeam === "team2" ? favTeamSkill : otherTeamSkill}</p>
-                            <p>*home team has an advantage of +20% applied</p>
+                            <p>{team1Name} skill total: {bookiesFavTeam === "team1" ? favTeamSkill : otherTeamSkill}{team1Home ? "*" : ""}</p>
+                            <p>{team2Name} skill total: {bookiesFavTeam === "team2" ? favTeamSkill : otherTeamSkill}{!team1Home ? "*" : ""}</p>
+                            <p>*home team advantage of +20% applied</p>
                         </div>
                     }
                 </div>
