cb09dbd4cd4652e82dab91ac9ddf4a3a94621db6
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-31 15:10:06 +0000

    Display Teams component created - final panel to render with all team info and bookies favourite

---
+++ src/Components/TeamsDisplay/index.js
@@ -0,0 +1,19 @@
+// imports
+import { connect } from 'react-redux';
+import TeamsDisplay from './TeamsDisplay';
+
+
+// map state
+const mapStateToProps = ({ team1Colour, team2Colour, team1Name, team2Name, team1Home }) => {
+    return {
+        team1Colour,
+        team2Colour,
+        team1Name,
+        team2Name,
+        team1Home: team1Home ? "(Home)" : "(Away)",
+        team2Home: !team1Home ? "(Home)" : "(Away)",
+    }
+};
+
+
+export default connect(mapStateToProps)(TeamsDisplay);
