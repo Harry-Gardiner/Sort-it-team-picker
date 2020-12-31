08c3f71d0c217e6d6d9fc014d2dfeea43b0bfb3e
Harry Gardiner <gardiner_harry@yahoo.com>
2020-12-31 13:10:41 +0000

    Header component created reset button

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+// imports 
+import ResetBtn from '../Reset';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Sort-it</h1>
+            <p>Team selector</p>
+            <nav>
+                <ResetBtn />
+            </nav>
+        </div>
+    )
+}
+
+export default Header; 
